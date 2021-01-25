394c0e41007ce3b951160fb712bf3a52394e4801
jiapei.xie <2170928274@qq.com>
2021-01-25 17:41:00 +0800

    Demo

--- AlamofireSourceCodeAnalysis/AlamofireSourceCodeAnalysis/ViewController.swift
+++ AlamofireSourceCodeAnalysis/AlamofireSourceCodeAnalysis/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewDidLoad()
+    {
+        super .viewDidLoad()
+     
+        // 请求网络
+        requestNetwork()
     }
+    
+    // 请求网络
+    func requestNetwork()
+    {
+        let urlString = "https://www.baidu.com"
 
-
+        AF.request(urlString).response
+        { (response) in
+            debugPrint(response)
+        }
+    }
+    
+    // 响应处理
+    func responseProcessing()
+    {
+        AF.request("https://httpbin.org/get").responseJSON
+        { response in
+            debugPrint(response)
+        }
+    }
 }
 
